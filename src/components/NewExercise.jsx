a2a0c753dff3a69e3ce767d63f4a7daf70427178
lillykml <64362067+lillykml@users.noreply.github.com>
2024-06-02 13:19:36 +0200

    Styled new form

--- src/components/NewExercise.jsx
+++ src/components/NewExercise.jsx
@@ -1,19 +1,21 @@
 const NewExercise = ({name, repetitions, nameHandler, repetitionsHandler, addExercise}) => {
     return (
-        <>
-        <h3>Add a new exercises</h3>
-        <form onSubmit={addExercise}>
-            <div>
-                <label>Name</label>
-                <input value={name} onChange={nameHandler}/>
-            </div>
-            <div>
-                <label>Repetitions</label>
-                <input value={repetitions} onChange={repetitionsHandler}/>
-            </div>
-            <button type="submit">Add</button>
-        </form>
-        </>
+            <>
+                <h3>Add a new exercises to the database</h3>
+                <div className="new-exercise-container">
+                    <form onSubmit={addExercise}>
+                        <div className="form-group">
+                            <label>Name</label>
+                            <input value={name} onChange={nameHandler}/>
+                        </div>
+                        <div className="form-group">
+                            <label>Repetitions</label>
+                            <input value={repetitions} onChange={repetitionsHandler}/>
+                        </div>
+                        <button type="submit">Add</button>
+                    </form>
+                </div>
+            </>
     )
 }
 
